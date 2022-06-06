24a297d97c1fe5375e21de7e293025ed8c1841d9
Ravindran8499 <ravindransri01@gmail.com>
2022-06-06 13:14:10 +0530

    Transaction history page added

---
+++ transactionhistory.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction History</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+</head>
+
+<body>
+
+<?php
+  include 'navbar.php';
+?>
+
+	<div class="container">
+        <h2 class="text-center pt-4">Transaction History</h2>
+        
+       <br>
+       <div class="table-responsive-sm">
+    <table class="table table-hover table-striped table-condensed table-bordered">
+        <thead>
+            <tr>
+                <th class="text-center">S.No.</th>
+                <th class="text-center">Sender</th>
+                <th class="text-center">Receiver</th>
+                <th class="text-center">Amount</th>
+                <th class="text-center">Date & Time</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+
+            include 'config.php';
+
+            $sql ="SELECT * FROM transaction";
+
+            $query =mysqli_query($link, $sql);
+            if(!$query){
+                die(mysqli_error($link));
+            }
+            while($rows = mysqli_fetch_assoc($query))
+            {
+        ?>
+
+            <tr>
+            <td class="py-2"><?php echo $rows['sno']; ?></td>
+            <td class="py-2"><?php echo $rows['sender']; ?></td>
+            <td class="py-2"><?php echo $rows['receiver']; ?></td>
+            <td class="py-2"><?php echo $rows['balance']; ?> </td>
+            <td class="py-2"><?php echo $rows['date_time']; ?> </td>
+                
+        <?php
+            }
+
+        ?>
+        </tbody>
+    </table>
+
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
