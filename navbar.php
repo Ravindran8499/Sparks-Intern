73ab3d3bbdcead69c921bf0bca7a9c446cfc5e74
Ravindran8499 <ravindransri01@gmail.com>
2022-06-06 13:15:34 +0530

    Update color of navbar

--- navbar.php
+++ navbar.php
@@ -1,9 +1,10 @@
 <nav class="navbar navbar-expand-lg bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand " href="./index.php"><img class="bank-image" src="./img/bankimage2.png" alt="" srcset=""></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <div class="container-fluid bg-dark">
+    <a class="navbar-brand" href="./index.php"><img class="bank-image" src="./img/bankimage2.png" alt="" srcset=""></a>
+    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    <!--All uls and lis -->
     <div class="collapse navbar-collapse pt-0" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
@@ -19,11 +20,6 @@
           <a class="nav-link  text-white" href="./transactionhistory.php">Transaction History</a>
         </li>
       </ul>
-      <!-- <div class="d-flex" role="search">
-      <li class="d-flex nav-item">
-          <a class="nav-link" href="./index.php"><img class="bank-image" src="./img/bankimage2.png" /></a>
-        </li>
-      </div> -->
     </div>
   </div>
 </nav>
